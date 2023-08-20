a028160c42eab33b9f41d3a8d3c95a8230c75063
MOHAMMED TAHER <100471051+samhamed@users.noreply.github.com>
2023-08-20 13:31:43 +0100

    update

--- src/components/DatesList.js
+++ src/components/DatesList.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import React from "react";
+import { Row, Col } from "react-bootstrap";
 const DatesList = ({ person }) => {
-	return (
-		<Row className='justify-content-center my-2'>
-			<Col sm='8' className=''>
-				<div className='rectangle p-2'>
-					{person.length ? (
-						person.map((item) => {
-							return (
-								<div key={item.id} className='d-flex  border-bottom mx-3'>
-									<img className='img-avatar' alt='img1' src={item.img}></img>
-									<div className='px-3'>
-										<p className='d-inline fs-5'> {item.name}</p>
-										<p className='fs-6'> {item.date}</p>
-									</div>
-								</div>
-							);
-						})
-					) : (
-						<h2 className='p-5 text-center'>لا يوجد مواعيد</h2>
-					)}
-				</div>
-			</Col>
-		</Row>
-	);
+  return (
+    <Row className="justify-content-center my-2">
+      <Col sm="8" className="">
+        <div className="rectangle p-2">
+          {person.length ? (
+            person.map((item) => {
+              return (
+                <div key={item.id} className="d-flex  border-bottom mx-3">
+                  <img className="img-avatar" alt="img1" src={item.img}></img>
+                  <div className="px-3">
+                    <p className="d-inline fs-5"> {item.name}</p>
+                    <p className="fs-6"> {item.date}</p>
+                  </div>
+                </div>
+              );
+            })
+          ) : (
+            <h2 className="p-5 text-center">لا يوجد مواعيد</h2>
+          )}
+        </div>
+      </Col>
+    </Row>
+  );
 };
 
 export default DatesList;
